b637aa2aa541776377fb8c03c005ea8229a77ee4
Andre601 <Andre601@users.noreply.github.com>
2022-08-18 23:48:02 +0000

    deploy: 078c860d0436e3c6e0c000ca96d5e1528d373f23

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ch.andre601.fluxpoint4j"},{"l":"ch.andre601.fluxpoint4j.image"},{"l":"ch.andre601.fluxpoint4j.image.format"},{"l":"ch.andre601.fluxpoint4j.request"},{"l":"ch.andre601.fluxpoint4j.util"},{"l":"ch.andre601.fluxpoint4j.welcome"}]
